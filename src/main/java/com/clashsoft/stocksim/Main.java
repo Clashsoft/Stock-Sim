49c0765530b6838db2b885072ae47790f44f2415
Clashsoft <clashsoft@hotmail.com>
2017-12-29 00:36:43 +0100

    JavaFX Application
    - Made the `Main` class a JavaFX application.

--- src/main/java/com/clashsoft/stocksim/Main.java
+++ src/main/java/com/clashsoft/stocksim/Main.java
@@ -1,8 +1,26 @@
 package com.clashsoft.stocksim;
 
-public class Main
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application
 {
 	public static void main(String[] args)
 	{
+		launch(Main.class, args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		final Parent root = FXMLLoader.load(Main.class.getResource("ui/MainView.fxml"));
+
+		primaryStage.setTitle("Stock Simulator");
+		primaryStage.setScene(new Scene(root));
+
+		primaryStage.show();
 	}
 }
