0108174385f82d641353ae789e8f778b4b679c14
Clashsoft <clashsoft@hotmail.com>
2018-01-22 11:41:19 +0100

    Update Main View
    - Updated the Main View.

--- src/main/java/com/clashsoft/stocksim/Main.java
+++ src/main/java/com/clashsoft/stocksim/Main.java
@@ -1,5 +1,7 @@
 package com.clashsoft.stocksim;
 
+import com.clashsoft.stocksim.local.LocalStockSim;
+import com.clashsoft.stocksim.ui.MainViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,7 +18,15 @@ public class Main extends Application
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		final Parent root = FXMLLoader.load(Main.class.getResource("ui/MainView.fxml"));
+		final LocalStockSim stockSim = new LocalStockSim();
+
+		final FXMLLoader loader = new FXMLLoader(Main.class.getResource("ui/MainView.fxml"));
+		final Parent root = loader.load();
+
+		final MainViewController controller = loader.getController();
+
+		controller.setStockSim(stockSim);
+		controller.setStage(primaryStage);
 
 		primaryStage.setTitle("Stock Simulator");
 		primaryStage.setScene(new Scene(root));
--- src/main/java/com/clashsoft/stocksim/ui/MainViewController.java
+++ src/main/java/com/clashsoft/stocksim/ui/MainViewController.java
@@ -1,34 +1,126 @@
 package com.clashsoft.stocksim.ui;
 
+import com.clashsoft.stocksim.Main;
+import com.clashsoft.stocksim.model.Player;
+import com.clashsoft.stocksim.model.Stock;
+import com.clashsoft.stocksim.model.StockSim;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainViewController
 {
 	@FXML
-	private TableView stocksTable;
+	private TableView<Stock> stocksTable;
 
 	@FXML
-	private TableColumn stockSymbolColumn;
+	private TableColumn<Stock, String> stockSymbolColumn;
 	@FXML
-	private TableColumn stockNameColumn;
+	private TableColumn<Stock, String> stockNameColumn;
 	@FXML
-	private TableColumn stockPriceColumn;
+	private TableColumn<Stock, Long> stockPriceColumn;
 	@FXML
-	private TableColumn stockAbsChangeColumn;
+	private TableColumn<Stock, Long> stockAbsChangeColumn;
 	@FXML
-	private TableColumn stockRelChangeColumn;
+	private TableColumn<Stock, Double> stockRelChangeColumn;
 
 	@FXML
-	private TableView playersTable;
+	private TableView<Player> playersTable;
 
 	@FXML
-	private TableColumn playerNameColumn;
+	private TableColumn<Player, String> playerNameColumn;
 	@FXML
-	private TableColumn playerNetWorthColumn;
+	private TableColumn<Player, Long> playerNetWorthColumn;
 	@FXML
-	private TableColumn playerAbsChangeColumn;
+	private TableColumn<Player, Long> playerAbsChangeColumn;
 	@FXML
-	private TableColumn playerRelChangeColumn;
+	private TableColumn<Player, Double> playerRelChangeColumn;
+
+	private StockSim sim;
+	private Stage    stage;
+
+	public MainViewController()
+	{
+	}
+
+	public MainViewController(StockSim sim)
+	{
+		this.sim = sim;
+	}
+
+	public void setStockSim(StockSim sim)
+	{
+		this.sim = sim;
+	}
+
+	public void setStage(Stage stage)
+	{
+		this.stage = stage;
+	}
+
+	@FXML
+	public void updateStocks()
+	{
+		this.stocksTable.getItems().clear();
+		this.stocksTable.getItems().addAll(this.sim.getStocks());
+	}
+
+	@FXML
+	public void updatePlayers()
+	{
+		this.playersTable.getItems().clear();
+		this.playersTable.getItems().addAll(this.sim.getPlayers());
+	}
+
+	@FXML
+	public void onAddStockAction()
+	{
+		try
+		{
+			final FXMLLoader loader = new FXMLLoader(Main.class.getResource("ui/CreateStockView.fxml"));
+			final Parent parent = loader.load();
+			final CreateStockViewController controller = loader.getController();
+			final Stage stage = new Stage();
+
+			controller.setStockSim(this.sim);
+			controller.setStage(stage);
+
+			stage.setScene(new Scene(parent));
+			stage.setTitle("Create New Stock");
+			stage.show();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	public void onAddPlayerAction()
+	{
+		try
+		{
+			final FXMLLoader loader = new FXMLLoader(Main.class.getResource("ui/CreatePlayerView.fxml"));
+			final Parent parent = loader.load();
+			final CreatePlayerViewController controller = loader.getController();
+			final Stage stage = new Stage();
+
+			controller.setStockSim(this.sim);
+			controller.setStage(stage);
+
+			stage.setScene(new Scene(parent));
+			stage.setTitle("Create New Player");
+			stage.show();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
